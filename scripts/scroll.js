f1114c797255b2ad24860fbcbeed6665395a758e
Slawa <slawapocht4@yandex.ru>
2022-01-21 19:52:28 +0400

    day one: added scrollToSections

---
+++ scripts/scroll.js
@@ -0,0 +1,33 @@
+const links = document.querySelectorAll('.header-menu__item a')
+const fullCharacteristicsBtn = document.querySelector('.card-details__link-characteristics')
+
+seamless.polyfill();
+
+const scrollToSection = (evt) => {
+    const target = evt.target
+    const id = target.getAttribute('href').substring(1)
+    const sectionName = evt.target.textContent
+    const section = document.getElementById(id)
+
+    if (section) {
+        seamless.elementScrollIntoView(section, {
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+        });
+    } else {
+        console.warn('Секция: ' + '"' + sectionName + '"' + ' отсутствует')
+    }
+}
+
+links.forEach(link => {
+    link.addEventListener('click', evt => {
+        evt.preventDefault()
+        scrollToSection(evt)
+    })
+})
+
+fullCharacteristicsBtn.addEventListener('click', evt => {
+    evt.preventDefault()
+    scrollToSection(evt)
+})
